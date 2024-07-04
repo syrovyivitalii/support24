3fcf7acdd76840b6dc521ea32b4ccb9df038ba2b
Syrovyi Vitalii <syrovyivitalii@gmail.com>
2024-07-04 11:09:58 +0300

    changed mapping for userController

--- src/main/java/lv/dsns/support24/user/controller/UserController.java
+++ src/main/java/lv/dsns/support24/user/controller/UserController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/users")
 @CrossOrigin(origins = "http://localhost:5173")
 public class UserController {
     private final UserServiceImpl userService;
